d9a179196c9ae482edd12e0f33496aa5d3cbe756
Pleshw <pleshw@outlook.com>
2020-06-24 22:21:28 -0300

    Cor padrão setada para transparente

--- Program.cs
+++ Program.cs
@@ -19,34 +19,37 @@ namespace maze
 
 
       canvas.Thickness = 1;
-      canvas.VerticalParallel(0, 0, 3, 4, Color.White);
-      canvas.HorizontalParallel(0, 5, 3, 4, Color.White);
 
-      canvas.Square(8, 0, 5, Color.White);
+      canvas.CurrentColor = Color.Black;
+
+      canvas.VerticalParallel(0, 0, 3, 4);
+      canvas.HorizontalParallel(0, 5, 3, 4);
+
+      canvas.Square(8, 0, 5);
 
 
       canvas.Thickness = 2;
-      canvas.Square(8, 7, 5, Color.White);
+      canvas.Square(8, 7, 5);
 
 
-      canvas.Pixel(16, 0, Color.White);
-      canvas.Pixel(16, 2, Color.White);
-      canvas.Pixel(16, 4, Color.White);
-      canvas.Pixel(16, 6, Color.White);
-      canvas.Pixel(16, 8, Color.White);
-      canvas.Pixel(16, 10, Color.White);
-      canvas.Pixel(16, 12, Color.White);
-      canvas.Pixel(16, 14, Color.White);
+      canvas.Pixel(16, 0, Color.Red);
+      canvas.Pixel(16, 2, Color.Red);
+      canvas.Pixel(16, 4, Color.Red);
+      canvas.Pixel(16, 6, Color.Red);
+      canvas.Pixel(16, 8, Color.Red);
+      canvas.Pixel(16, 10, Color.Red);
+      canvas.Pixel(16, 12, Color.Red);
+      canvas.Pixel(16, 14, Color.Red);
 
-      canvas.Pixel(0, 16, Color.White);
-      canvas.Pixel(2, 16, Color.White);
-      canvas.Pixel(4, 16, Color.White);
-      canvas.Pixel(6, 16, Color.White);
-      canvas.Pixel(8, 16, Color.White);
-      canvas.Pixel(10, 16, Color.White);
-      canvas.Pixel(12, 16, Color.White);
-      canvas.Pixel(14, 16, Color.White);
-      canvas.Pixel(16, 16, Color.White);
+      canvas.Pixel(0, 16, Color.Red);
+      canvas.Pixel(2, 16, Color.Red);
+      canvas.Pixel(4, 16, Color.Red);
+      canvas.Pixel(6, 16, Color.Red);
+      canvas.Pixel(8, 16, Color.Red);
+      canvas.Pixel(10, 16, Color.Red);
+      canvas.Pixel(12, 16, Color.Red);
+      canvas.Pixel(14, 16, Color.Red);
+      canvas.Pixel(16, 16, Color.Red);
 
       canvas.SavePNG("test.png");
     }
--- render/Canvas.cs
+++ render/Canvas.cs
@@ -10,7 +10,7 @@ namespace PleshwGraphics
     public readonly int Width;
     public readonly int Height;
 
-    public static Color DefaultPaintColor = Color.FromArgb(0, 0, 0);
+    public Color CurrentColor = Color.Transparent;
 
     private Bitmap _bitmap;
 
@@ -32,7 +32,7 @@ namespace PleshwGraphics
     public void Pixel(int x, int y, Color color) => _bitmap.SetPixel(x, y, color);
     public void Pixel(Point p, Color color) => Pixel(p.X, p.Y, color);
 
-    public void Clear() => Clear(DefaultPaintColor);
+    public void Clear() => Clear(CurrentColor);
     public void Clear(Color c)
     {
       for (int y = 0; y < Height; y++)
@@ -41,9 +41,9 @@ namespace PleshwGraphics
     }
 
 
-    public void VerticalLine(Point p, int size) => VerticalLine(p, size, DefaultPaintColor);
+    public void VerticalLine(Point p, int size) => VerticalLine(p, size, CurrentColor);
     public void VerticalLine(Point p, int size, Color c) => VerticalLine(p.X, p.Y, size, c);
-    public void VerticalLine(int x, int y, int size) => VerticalLine(x, y, size, DefaultPaintColor);
+    public void VerticalLine(int x, int y, int size) => VerticalLine(x, y, size, CurrentColor);
     public void VerticalLine(int x, int y, int size, Color c)
     {
       for (int thick = 0; thick < Thickness; thick++)
@@ -51,9 +51,9 @@ namespace PleshwGraphics
           Pixel(x + thick, y + i, c);
     }
 
-    public void HorizontalLine(Point p, int size) => HorizontalLine(p, size, DefaultPaintColor);
+    public void HorizontalLine(Point p, int size) => HorizontalLine(p, size, CurrentColor);
     public void HorizontalLine(Point p, int size, Color c) => HorizontalLine(p.X, p.Y, size, c);
-    public void HorizontalLine(int x, int y, int size) => HorizontalLine(x, y, size, DefaultPaintColor);
+    public void HorizontalLine(int x, int y, int size) => HorizontalLine(x, y, size, CurrentColor);
     public void HorizontalLine(int x, int y, int size, Color c)
     {
       for (int thick = 0; thick < Thickness; thick++)
@@ -62,27 +62,27 @@ namespace PleshwGraphics
     }
 
 
-    public void HorizontalParallel(Point p, int length, int distance) => HorizontalParallel(p, length, distance, DefaultPaintColor);
+    public void HorizontalParallel(Point p, int length, int distance) => HorizontalParallel(p, length, distance, CurrentColor);
     public void HorizontalParallel(Point p, int length, int distance, Color c) => HorizontalParallel(p.X, p.Y, length, distance, c);
-    public void HorizontalParallel(int x, int y, int length, int distance) => HorizontalParallel(x, y, length, distance, DefaultPaintColor);
+    public void HorizontalParallel(int x, int y, int length, int distance) => HorizontalParallel(x, y, length, distance, CurrentColor);
     public void HorizontalParallel(int x, int y, int length, int distance, Color c)
     {
       HorizontalLine(x, y, length, c);
       HorizontalLine(x, y + distance, length, c);
     }
 
-    public void VerticalParallel(Point p, int length, int distance) => VerticalParallel(p, length, distance, DefaultPaintColor);
+    public void VerticalParallel(Point p, int length, int distance) => VerticalParallel(p, length, distance, CurrentColor);
     public void VerticalParallel(Point p, int length, int distance, Color c) => VerticalParallel(p.X, p.Y, length, distance, c);
-    public void VerticalParallel(int x, int y, int length, int distance) => VerticalParallel(x, y, length, distance, DefaultPaintColor);
+    public void VerticalParallel(int x, int y, int length, int distance) => VerticalParallel(x, y, length, distance, CurrentColor);
     public void VerticalParallel(int x, int y, int length, int distance, Color c)
     {
       VerticalLine(x, y, length, c);
       VerticalLine(x + distance, y, length, c);
     }
 
-    public void Rectangle(Point p, int w, int h) => Rectangle(p, w, h, DefaultPaintColor);
+    public void Rectangle(Point p, int w, int h) => Rectangle(p, w, h, CurrentColor);
     public void Rectangle(Point p, int w, int h, Color c) => Rectangle(p.X, p.Y, w, h, c);
-    public void Rectangle(int x, int y, int w, int h) => Rectangle(x, y, w, h, DefaultPaintColor);
+    public void Rectangle(int x, int y, int w, int h) => Rectangle(x, y, w, h, CurrentColor);
     public void Rectangle(int x, int y, int width, int height, Color c)
     {
       int offset = Thickness - 1;
